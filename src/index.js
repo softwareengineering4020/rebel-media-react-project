0ecb2c0363f664ed9e30d29b9fee5d8ffda0fc64
Nikolas Lennon <nl16g@my.fsu.edu>
2018-10-12 13:56:25 -0500

    Complete wireframing of homepage, with extra features such as cards and navigation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HomePage from './components/Homepage/HomePage';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render( <HomePage />, document.getElementById('root'));
